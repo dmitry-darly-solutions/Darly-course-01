716080bc8fd662fad6b5d53d300313af26801893
Andrii Chuprina <andriichuprina@mac.local>
2020-09-30 16:20:57 +0300

    Created and fullfilled moveelement.js

---
+++ javascript-arrays/moveelement.js
@@ -0,0 +1,37 @@
+let moveCounter = 0;
+let arrayBefore = [], arrayAfter = [], arrayFromTo = [] ;
+let fromPos, toPos, element;
+
+function addElementToArrayMove() {
+    arrayBefore[moveCounter] = document.getElementById('element-move').value;
+    displayArrayMoveBefore();
+    moveCounter++;
+}
+function moveElement() {
+    arrayFromTo = document.getElementById('element-move').value;
+    arrayFromTo = (arrayFromTo.toString()).split(',');
+    fromPos = arrayFromTo[0];
+    toPos = arrayFromTo[1];
+    fromPositionToAnother(fromPos,toPos);
+    displayArrayMoveAfter();
+}
+function fromPositionToAnother(fromPosition,toPosition) {
+    if(fromPosition<0) {
+        fromPosition = arrayBefore.length - Math.abs(fromPosition);
+    }
+    if(toPosition<0) {
+        toPosition = arrayBefore.length - Math.abs(toPosition);
+    }
+    console.log(fromPosition,toPosition);
+    element = arrayBefore[fromPosition];
+    arrayAfter = arrayBefore.slice();
+    arrayAfter.splice(fromPosition,1);
+    arrayAfter.splice(toPosition,0,element);
+    console.log(element);
+}
+function displayArrayMoveBefore() {
+    document.getElementById('move-before').innerHTML = arrayBefore.join();
+}
+function displayArrayMoveAfter() {
+    document.getElementById('move-after').innerHTML = arrayAfter.join();
+}
