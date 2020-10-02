41bd6280e7c954b313871f74cb050f1172adcc18
Andrii Chuprina <andriichuprina@mac.local>
2020-10-02 12:02:57 +0300

    Kebab-case applied and minor fixes to  sum-of-two.js

--- js-arrays/sum-of-two.js
+++ js-arrays/sum-of-two.js
@@ -1,18 +1,15 @@
-let counterFirst = 0, counterSecond = 0;
-let arrayFirst = [], arraySecond = [], arraySum = [] ;
+const arrayFirst = [], arraySecond = [], arraySum = [] ;
 const elementSum = document.getElementById('element-sum');
 function addElementToArrayFirst() {
-    arrayFirst[counterFirst] = elementSum.value;
+    arrayFirst.push(elementSum.value);
     document.getElementById('result-sum-1').innerHTML = arrayFirst.join();
-    counterFirst++;
 }
 function addElementToArraySecond() {
-    arraySecond[counterSecond] = elementSum.value;
+    arraySecond.push(elementSum.value);
     document.getElementById('result-sum-2').innerHTML = arraySecond.join();
-    counterSecond++;
 }
 function displayArrayAll() {
-    let length = (arrayFirst.length >= arraySecond.length) ? arrayFirst.length : arraySecond.length;
+    let length = Math.max(arrayFirst.length,arraySecond.length);
     for ( let counterSum = 0; counterSum < length; counterSum++) {
         arraySum[counterSum] = (+arrayFirst[counterSum] || 0) + (+arraySecond[counterSum] || 0);
         document.getElementById('result-element-sum').innerHTML = arraySum.join();
