03f3e202754cba07251ea3e327726b10ca08ee18
Andrii Chuprina <andriichuprina@mac.local>
2020-10-15 19:39:02 +0300

    Minor addings to ulr-parse.js

--- js/scripts/object-methods/url-parse.js
+++ js/scripts/object-methods/url-parse.js
@@ -1,4 +1,11 @@
 function parseURL(){
-    document.getElementById('object-parse-result').innerText;
+    const url = document.getElementById('object-parse-input').value;
+    //const host =
+    //const hostname =
+    //const port =
+    //const protocol =
+    //const search =
+    //const password =
+    //const username =
     console.log(new URL('https://dmitripavlutin.com/parse-url-javascript/'));
 }
