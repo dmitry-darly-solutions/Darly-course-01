6a92e6588f52424e5ec4efdf87f8c9a61ec3ead8
Andrii Chuprina <andriichuprina@mac.local>
2020-10-15 16:57:53 +0300

    Changed html style (added input fields, checkbox and separate buttons)

--- js/scripts/object-methods/reading-status.js
+++ js/scripts/object-methods/reading-status.js
@@ -9,19 +9,22 @@ $(function(){
         }
     }
     $('#object-reading-add').click(function (){
-        let result = $('#object-reading-input').val().split(':');
-        books.push(new Book(result[0],result[1],result[2]));
+        let author = $('#object-reading-input-author').val();
+        let title = $('#object-reading-input-title').val();
+        let readingStatus = $('#object-reading-input-reading').is(':checked');
+        books.push(new Book(author,title,readingStatus));
     });
     $('#object-reading-show').click(function () {
-        $('#result-object-reading-property').text($('#object-reading-input').val()+':');
+        let selectedByProperty = $('#object-reading-list').val();
+        $('#result-object-reading-property').text(selectedByProperty+':');
         books.forEach(function (item){
             Object.keys(item).forEach(function(key) {
-                if(key===$('#object-reading-input').val()) {
+                if(key===selectedByProperty) {
                     properties.push(item[key]);
                 }
             });
         });
-        $('#result-object-reading').text(properties);
+        $('#result-object-reading').text( properties);
         properties=[];
     });
 });
