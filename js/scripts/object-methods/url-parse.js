e6ea86fc829ddd60559960da0195e77492053bfd
Andrii Chuprina <andriichuprina@mac.local>
2020-10-16 15:24:00 +0300

    Added js code to parse-url.js and elemetns to index.html

--- js/scripts/object-methods/url-parse.js
+++ js/scripts/object-methods/url-parse.js
@@ -1,11 +1,19 @@
-function parseURL(){
-    const url = document.getElementById('object-parse-input').value;
-    //const host =
-    //const hostname =
-    //const port =
-    //const protocol =
-    //const search =
-    //const password =
-    //const username =
-    console.log(new URL('https://dmitripavlutin.com/parse-url-javascript/'));
+function parseURL() {
+    const inputUrl = document.getElementById('object-parse-input').value;
+    const result = inputUrl.match(/((^\w{2,5}:)\/{2}(([А-Яа-я\w-\.]+):?([0-9]{0,5})))(\/?[А-Яа-я\w-\.\/]+)/);
+    const host = result[3];
+    const hostname = result[4];
+    const href = inputUrl;
+    const origin = result[1];
+    const pathname = result[6];
+    const port = result[5];
+    const protocol = result[2];
+    document.getElementById('object-parse-result').innerHTML = `<div>host: ${host}<\div>
+                                                                <div>hostname: ${hostname}<\div>
+                                                                <div>href: ${href}<\div>
+                                                                <div>origin: ${origin}<\div>
+                                                                <div>pathname: ${pathname}<\div>
+                                                                <div>port: ${port}<\div>
+                                                                <div>protocol: ${protocol}<\div>`;
+    console.log("logging");
 }
