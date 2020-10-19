0fa54450f09d1988458c8244add78a80c55df1f7
Andrii Chuprina <andriichuprina@mac.local>
2020-10-19 15:19:37 +0300

    Fixes

--- js/scripts/object-methods/check-is-it-dom-element.js
+++ js/scripts/object-methods/check-is-it-dom-element.js
@@ -1,19 +1,18 @@
-function showDOM(){
+function showDOM() {
     document.getElementById('check-result').innerText = document.documentElement.innerHTML;
 }
-function checkIsDOMElement(){
+function checkIsDOMElement() {
     const inputValue = document.getElementById('check-input').value;
     const getElementById = document.getElementById(inputValue);
     const getElementsByName = document.getElementsByName(inputValue);
     const getElementsByClassName = document.getElementsByClassName(inputValue);
     const getElementsByTagName = document.getElementsByTagName(inputValue);
     let element = 0;
-    if(getElementById!==null) element++;
-    if(getElementsByName.length) element+=getElementsByName.length;
-    if(getElementsByClassName.length) element+=getElementsByClassName.length;
-    if(getElementsByTagName.length) element+=getElementsByTagName.length;
-    if(element)
-        element>1?alert(`${inputValue} is in DOM ${element} times`):
-            alert(`${inputValue} is in DOM ${element} time`);
-
+    if (getElementById !== null) element++;
+    if (getElementsByName.length) element += getElementsByName.length;
+    if (getElementsByClassName.length) element += getElementsByClassName.length;
+    if (getElementsByTagName.length) element += getElementsByTagName.length;
+    if (!element) return;
+    if (element)
+        alert(`${inputValue} is in DOM ${element > 1 ? 'times' : 'time'} `);
 }
