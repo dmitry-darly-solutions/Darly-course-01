0d37fa77266b5ce43865abe8c20e4a626eb80768
Andrii Chuprina <andriichuprina@mac.local>
2020-10-19 11:54:53 +0300

    Fixed

--- js/scripts/object-methods/reading-status.js
+++ js/scripts/object-methods/reading-status.js
@@ -1,5 +1,6 @@
-const books=new Array();
+const books = new Array();
 let properties = new Array();
+
 class Book {
     constructor(author, title, readingStatus) {
         this.author = author;
@@ -7,23 +8,24 @@ class Book {
         this.readingStatus = readingStatus;
     }
 }
+
 function addBook() {
     const author = document.getElementById('object-reading-input-author').value;
     const title = document.getElementById('object-reading-input-title').value;
     const readingStatus = document.getElementById('object-reading-input-reading').checked;
-    console.log(readingStatus);
-    books.push(new Book(author,title,readingStatus));
+    books.push(new Book(author, title, readingStatus));
 }
+
 function sortProperty() {
     const selectedByProperty = document.getElementById('object-reading-list').value;
     document.getElementById('result-object-reading-property').innerHTML = `${selectedByProperty}:`;
-    books.forEach(function (item){
-        Object.keys(item).forEach(function(key) {
-            if(key===selectedByProperty) {
+    books.forEach(function (item) {
+        Object.keys(item).forEach(function (key) {
+            if (key === selectedByProperty) {
                 properties.push(item[key]);
             }
         });
     });
-    document.getElementById('result-object-reading').innerHTML= properties.toString();
-    properties=[];
+    document.getElementById('result-object-reading').innerHTML = properties.toString();
+    properties = [];
 }
